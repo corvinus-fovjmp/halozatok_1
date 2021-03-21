772fadf9d13c99a2da36658b64e8678e2151afdc
Máté Bukovácz <mate.bukovacz@stud.uni-corvinus.hu>
2021-03-21 14:53:21 +0100

    színezés és pascal háromszög

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak_1.js
@@ -0,0 +1,47 @@
+﻿window.onload = function () {
+    let hova = document.getElementById("szines");
+
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        hova.appendChild(sor)
+        sor.classList.add("sor");
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            sor.appendChild(szám);
+            szám.classList.add("elem");
+            szám.innerText = (s + 1) * (o + 1);
+            szám.style.background = `rgb(${255 - (255 / 10 * s)}, 0, 0)`;
+            szám.style.color = `white`;
+        }
+    }
+
+
+    let oda = document.getElementById("pascal");
+
+    for (var psor = 0; psor < 10; psor++) {
+        //új div létrehozása az új sornak
+        //új div osztálylistájához add hozzá a "sor"-t
+        //új div-et add hozzá a "pascal" gyermekeihez
+        let ps = document.createElement("div");
+        oda.appendChild(ps);
+        ps.classList.add("psor");
+        for (var poszlop = 0; poszlop <= psor; poszlop++) {
+            let pelem = document.createElement("div");
+            ps.appendChild(pelem);
+            pelem.classList.add("pelem");
+            pelem.innerText = faktorialisR(psor) / (faktorialisR(poszlop) * faktorialisR(psor - poszlop));
+            if (poszlop == 0) {
+                pelem.style.marginLeft = (document.documentElement.clientWidth / 2) - 15 - (psor * 15) + ".px";
+            }
+        }
+    }
+}
+
+var faktorialisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * faktorialisR(n - 1);
+    }
+}
