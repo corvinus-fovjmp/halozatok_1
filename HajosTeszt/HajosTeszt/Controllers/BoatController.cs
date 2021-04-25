8300486f109aa6aa2aded233379151a84f48fe08
Máté Bukovácz <mate.bukovacz@stud.uni-corvinus.hu>
2021-04-25 16:27:27 +0200

    gyak9

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -8,14 +8,26 @@ namespace HajosTeszt.Controllers
 {
     public class BoatController : Controller
     {
+        
         [HttpGet]
-        [Route("/questions/all")]
+        [Route("/questions/{sorszám}")]
         public ActionResult M1()
         {
             HajostesztContext context = new HajostesztContext();
             var kérdések = from x in context.Questions select x.QuestionText;
 
             return new JsonResult(kérdések);
+        }
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
     }
 }
--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -20,7 +20,31 @@ function letöltésBefejezõdött(d) {
     console.log(d)
     kérdések = d
     kérdésMegjelenítés(0);
+
+
+
 }
+
+fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data)
+);
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );
+
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
+
+
 
 var kérdésMegjelenítés = function (kérdésSzáma) {
 
@@ -42,6 +66,31 @@ var kérdésMegjelenítés = function (kérdésSzáma) {
     válasz1.innerText = kérdések[kérdésSzáma].answer1
     válasz2.innerText = kérdések[kérdésSzáma].answer2
     válasz3.innerText = kérdések[kérdésSzáma].answer3
+}
+
+
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
+
+
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
 }
 
 
