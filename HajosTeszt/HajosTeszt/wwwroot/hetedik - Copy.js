ccdcba1783c59677640d5ce63d465164b6607bf9
Máté Bukovácz <mate.bukovacz@stud.uni-corvinus.hu>
2021-05-02 20:54:20 +0200

    10_gyak

--- HajosTeszt/HajosTeszt/wwwroot/hetedik - Copy.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik - Copy.js
@@ -7,11 +7,60 @@
 
 var kérdések;
 var kérdésSorszám = 0;
+var hotList = [];           
+var questionsInHotList = 3; 
+var displayedQuestion;      
+var numberOfQuestions;      
+var nextQuestion = 1;       
+var timeoutHandler;
+timeoutHandler = setTimeout(elõre, 3000);
 
 function letöltés() {
-    fetch('/questions.json')
-        .then(response => response.json())
-        .then(data => letöltésBefejezõdött(data));
+    fetch(`/questions/${questionNumber}`)
+        .then(
+            result => {
+                if (!result.ok) {
+                    console.error(`Hibás letöltés: ${response.status}`)
+                }
+                else {
+                    return result.json()
+                }
+            }
+        )
+        .then(
+            q => {
+                hotList[destination].question = q;
+                hotList[destination].goodAnswers = 0;
+                console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`)
+            }
+        );
+}
+function init() {
+    for (var i = 0; i < questionsInHotList; i++) {
+        let q = {
+            question: {},
+            goodAnswers: 0
+        }
+        hotList[i] = q;
+    }
+
+    
+    for (var i = 0; i < questionsInHotList; i++) {
+        kérdésBetöltés(nextQuestion, i);
+        nextQuestion++;
+    }
+}
+
+function elõre() {
+    displayedQuestion++;
+    if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+    kérdésMegjelenítés()
+}
+function elõre() {
+    clearTimeout(timeoutHandler)
+    displayedQuestion++;
+    if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+    kérdésMegjelenítés()
 }
 
 
@@ -36,15 +85,26 @@ fetch('/questions/1')
     );
 
 function kérdésMegjelenítés(kérdés) {
+
+    let kérdés = hotList[displayedQuestion].question;
     console.log(kérdés);
     document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
     document.getElementById("válasz1").innerText = kérdés.answer1
     document.getElementById("válasz2").innerText = kérdés.answer2
     document.getElementById("válasz3").innerText = kérdés.answer3
     document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+
+    .then(
+        q => {
+            hotList[destination].question = q;
+            hotList[destination].goodAnswers = 0;
+            console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`)
+            if (displayedQuestion == undefined && destination == 0) { //!!!!!!!!!!!!!
+                displayedQuestion = 0;
+                kérdésMegjelenítés();
+            }
+
 }
-
-
 
 
 
@@ -94,7 +154,6 @@ function válaszfeldolgozás(válasz) {
         return válasz.json()
     }
 }
-
 
 
 window.onload = () => {
